d1e25cd6fbc5cac692c785678de6818cf1173113
Neubert, Laurin <laurin.neubert@study.thws.de>
2024-04-04 18:54:26 +0200

    added switch and design for dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {ThemeToggleComponent} from "./theme-toggle/theme-toggle.component";
+import {ThemeService} from "./theme.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HomeComponent],
+  imports: [RouterModule, HomeComponent, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipeCollection';
+
+  constructor(private themeService: ThemeService) {
+  }
+
+  ngOnInit() {
+    const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.themeService.setDarkMode(prefersDark);
+  }
+
+
 }
