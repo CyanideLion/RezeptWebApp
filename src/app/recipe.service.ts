373a546946872772ca482e90479add01239ee410
Neubert, Laurin <laurin.neubert@study.thws.de>
2024-04-04 11:19:05 +0200

    changed dummy data to comply with new datatype

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -10,7 +10,7 @@ export class RecipeService {
       "id": 1,
       "name": "Gregory",
       "photo": "/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
-      "ingredients": "voluptate",
+      "ingredients": ["voluptate"],
       "steps": "Velit nulla Lorem incididunt adipisicing et nulla laborum occaecat tempor nostrud. Excepteur sunt sunt ad anim reprehenderit irure non ea. Fugiat amet officia laborum enim quis minim nostrud ad. Commodo ut ea consequat ea irure deserunt cupidatat.\r\n",
       "durationTotal": 29,
       "durationPrep": 31,
@@ -26,7 +26,9 @@ export class RecipeService {
       "id": 2,
       "name": "Mcintosh",
       "photo": "/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg",
-      "ingredients": "occaecat",
+      "ingredients": [
+        "Eier", "Mehl", "Milch"
+      ],
       "steps": "Fugiat pariatur incididunt labore et ut incididunt labore labore esse dolore aliqua. Non consectetur commodo id sint excepteur ipsum quis ea aliquip laborum ad elit excepteur. Dolor cillum laborum tempor excepteur dolore.\r\n",
       "durationTotal": 22,
       "durationPrep": 23,
@@ -42,7 +44,7 @@ export class RecipeService {
       "id": 3,
       "name": "Sharron",
       "photo": "/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
-      "ingredients": "quis",
+      "ingredients": ["quis"],
       "steps": "Eu eu occaecat non exercitation commodo esse. Reprehenderit adipisicing in consectetur nisi Lorem nisi aliquip velit mollit. Veniam sint ullamco est sit fugiat eu cupidatat ea aliquip. Amet magna elit laboris magna quis aliquip adipisicing. Nulla occaecat labore irure quis nulla magna.\r\n",
       "durationTotal": 31,
       "durationPrep": 40,
@@ -58,7 +60,7 @@ export class RecipeService {
       "id": 4,
       "name": "Ashley",
       "photo": "/assets/r-architecture-GGupkreKwxA-unsplash.jpg",
-      "ingredients": "labore",
+      "ingredients": ["labore"],
       "steps": "Mollit adipisicing incididunt magna id aliqua. Lorem proident sint consectetur eiusmod in tempor eu aliqua ullamco magna laboris deserunt aute. Id anim dolore velit nulla amet exercitation anim eu pariatur ut culpa. Labore id id laborum commodo dolor sit nulla eu. Cillum ipsum sint anim et sit sint aliqua ad nisi excepteur. Ut ut Lorem occaecat excepteur.\r\n",
       "durationTotal": 36,
       "durationPrep": 37,
@@ -74,7 +76,7 @@ export class RecipeService {
       "id": 5,
       "name": "Cox",
       "photo": "/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
-      "ingredients": "cupidatat",
+      "ingredients": ["cupidatat"],
       "steps": "Veniam incididunt non ex fugiat incididunt consequat adipisicing culpa sit ullamco. Aliquip consequat velit laboris velit veniam dolore magna deserunt. Ad reprehenderit incididunt nulla proident nulla non consectetur.\r\n",
       "durationTotal": 34,
       "durationPrep": 29,
@@ -97,6 +99,10 @@ export class RecipeService {
 
   getRecipeByID(id: number): Recipe | undefined {
     return this.recipeList.find(recipe => recipe.id === id);
+  }
+
+  getAllIngredients(id: number): string[] | undefined {
+    return this.getRecipeByID(id)?.ingredients;
   }
 
 }
