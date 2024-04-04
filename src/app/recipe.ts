d30cac96b437bba7579ccf6596408c2e14557b28
Neubert, Laurin <laurin.neubert@study.thws.de>
2024-04-04 11:18:20 +0200

    changed datatype for ingredients

--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -2,7 +2,7 @@ export interface Recipe {
   id: number,
   name: string,
   photo: string,
-  ingredients: string, // might need to change this, depending on the implementation in the DB
+  ingredients: string[], // might need to change this, depending on the implementation in the DB
   steps: string,
   durationTotal: number,
   durationPrep: number,
