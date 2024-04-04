1b02a10f2d8ff282cfd120253189590d1a94238f
Neubert, Laurin <laurin.neubert@study.thws.de>
2024-04-04 11:19:52 +0200

    added functionality to new datatype of ingredients and changed design slightly

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,32 +2,44 @@ import {Component, inject} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {RecipeService} from "../recipe.service";
 import {Recipe} from "../recipe";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   template: `
     <article>
       <img class="listing-photo" [src]="recipe?.photo" alt="Foto von {{recipe?.name}}">
       <section class="listing-description">
-        <h2>{{ recipe?.name }}</h2>
+        <h2 class="listing-heading">{{ recipe?.name }}</h2>
         <p class="durationTotal">{{ recipe?.durationTotal }} Minuten</p>
+        <img *ngIf="recipe?.vegetarian" src="../../assets/425px-Vegetarian-mark.svg.png" class="icon" alt="Vegetarisch">
+        <img *ngIf="recipe?.vegan" src="../../assets/Vegan_friendly_icon.png" class="icon" alt="Vegan">
       </section>
       <section class="listing-features">
         <h2 class="section-heading">Mehr zu diesem Rezept</h2>
         <ul>
-          <li>Vorbereitungszeit: {{recipe?.durationPrep}}</li>
-          <li>Koch-/Backzeit: {{recipe?.durationCooking}}</li>
-          <li>Portionen: {{recipe?.servings}}</li>
-          <li>Vegetarisch: {{recipe?.vegetarian}}</li>
-          <li>Vegan: {{recipe?.vegan}}</li>
-          <li>Zutaten: {{recipe?.ingredients}}</li>
-          <li>Zubereitung: {{recipe?.steps}}</li>
-          <li>Hinweis: {{recipe?.notes}}</li>
-          <li>Rezept von: {{recipe?.source}}</li>
-          <li>Hinzugefügt am: {{recipe?.date}}</li>
+          <li>Vorbereitungszeit: {{ recipe?.durationPrep }} Minuten</li>
+          <li>Koch-/Backzeit: {{ recipe?.durationCooking }} Minuten</li>
+          <li>Portionen: {{ recipe?.servings }}</li>
+          <li>Vegetarisch: {{ recipe?.vegetarian }}</li>
+          <li>Vegan: {{ recipe?.vegan }}</li>
+          <li>Zutaten:
+            <ul>
+              <li *ngFor="let ingredient of recipe?.ingredients">{{ ingredient }}</li>
+            </ul>
+          </li>
+          <li>Zubereitung: {{ recipe?.steps }}</li>
+          <li>Hinweis: {{ recipe?.notes }}</li>
+          <li>Rezept von: {{ recipe?.source }}</li>
+          <li>Hinzugefügt am: {{ recipe?.date }}</li>
         </ul>
+
+
       </section>
     </article>
   `,
@@ -37,9 +49,11 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   recipeService = inject(RecipeService);
   recipe: Recipe | undefined;
+  ingredientList: string[] | undefined;
 
   constructor() {
     const recipeId = Number(this.route.snapshot.params['id']);
     this.recipe = this.recipeService.getRecipeByID(recipeId);
+    this.ingredientList  = this.recipeService.getAllIngredients(recipeId);
   }
 }
