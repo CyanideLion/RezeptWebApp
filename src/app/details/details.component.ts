6d88891d1cdb3fb4a277af2e13c1de30521cca3b
Neubert, Laurin <laurin.neubert@study.thws.de>
2024-04-04 18:54:50 +0200

    bunch of stuff I'm not smart enough to understand

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,25 +3,33 @@ import {ActivatedRoute} from "@angular/router";
 import {RecipeService} from "../recipe.service";
 import {Recipe} from "../recipe";
 import {NgForOf, NgIf} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    MatIcon
   ],
   template: `
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
     <article>
       <img class="listing-photo" [src]="recipe?.photo" alt="Foto von {{recipe?.name}}">
       <section class="listing-description">
         <h2 class="listing-heading">{{ recipe?.name }}</h2>
         <p class="durationTotal">{{ recipe?.durationTotal }} Minuten</p>
-        <img *ngIf="recipe?.vegetarian" src="../../assets/425px-Vegetarian-mark.svg.png" class="icon" alt="Vegetarisch">
-        <img *ngIf="recipe?.vegan" src="../../assets/Vegan_friendly_icon.png" class="icon" alt="Vegan">
+        <!--        <img *ngIf="recipe?.vegetarian" src="../../assets/425px-Vegetarian-mark.svg.png" class="icon" alt="Vegetarisch">-->
+<!--        <mat-icon *ngIf="recipe?.vegetarian" class="material-symbols-outlined">eco</mat-icon>-->
+        <span *ngIf="recipe?.vegetarian" class="material-symbols-outlined">eco</span>
+<!--        <mat-icon *ngIf="recipe?.vegan" class="material-symbols-outlined">psychiatry</mat-icon>-->
+        <span *ngIf="recipe?.vegan" class="material-symbols-outlined">temp_preferences_eco</span>
+        <!--        <img *ngIf="recipe?.vegan" src="../../assets/Vegan_friendly_icon.png" class="icon" alt="Vegan">-->
       </section>
       <section class="listing-features">
-        <h2 class="section-heading">Mehr zu diesem Rezept</h2>
+        <h2 class="section-heading">Zubereitung</h2>
         <ul>
           <li>Vorbereitungszeit: {{ recipe?.durationPrep }} Minuten</li>
           <li>Koch-/Backzeit: {{ recipe?.durationCooking }} Minuten</li>
