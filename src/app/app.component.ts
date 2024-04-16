f2720977c16d242e8efcd32d4e211821b3b4e71c
Laurin <laurin.neubert@ln13.de>
2024-04-16 11:58:42 +0200

    added Login Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import {RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ThemeToggleComponent} from "./theme-toggle/theme-toggle.component";
 import {ThemeService} from "./theme.service";
+import {LoginButtonComponent} from "./login-button/login-button.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HomeComponent, ThemeToggleComponent],
+  imports: [RouterModule, HomeComponent, ThemeToggleComponent, LoginButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,6 +23,4 @@ export class AppComponent implements OnInit {
     const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     this.themeService.setDarkMode(prefersDark);
   }
-
-
 }
